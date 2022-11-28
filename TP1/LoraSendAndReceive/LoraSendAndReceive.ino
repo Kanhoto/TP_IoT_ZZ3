45bab57ce30538586d76b217c7689209ab1aeff8
Vincent Canhoto <vincentkanoto@gmail.com>
2022-11-28 22:26:20 +0100

    [+]TP_IoT_ZZ3 - Writing AppKey into the NVM

--- TP1/LoraSendAndReceive/LoraSendAndReceive.ino
+++ TP1/LoraSendAndReceive/LoraSendAndReceive.ino
@@ -9,76 +9,90 @@
 ********************/
 bool enableHeater = false;
 uint8_t loopCnt = 0;
-//8248E25F4C09C294AD25ABDBA50343DC
-const char Crypt[] = {0x82, 0x48, 0xE2, 0x5F, 0x4C, 0x09, 0xC2, 0x94, 0xAD, 0x25, 0xAB, 0xDB, 0xA5, 0x03, 0x43, 0xDC};
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 LoRaModem modem;
-char key[] = SECRET_APP_KEY;
 String appEui = SECRET_APP_EUI;
 String appKey;
-
-/******************************************************************************************
-* Function : Binary_to_String                                                             *
-*                                                                                         *
-* param : Char *, int                                                                     *
-*                                                                                         *
-* Execution : Convert a char array of Hexadecimal into their ASCII equivalent in a String *
-*******************************************************************************************/
-String Binary_to_String(char * tab, const int _size){
-  String chaine = "";
-  for(int i = 0; i<_size; ++i){
-    char temp[2];
-    sprintf(temp, "%02X", tab[i]);
-    chaine += temp;
-  }
-  chaine.toUpperCase();
-  return chaine;
-}
-
-/********************************************************
-* Function : XorOp                                      *
-*                                                       *
-* param : Char *, const char *, const int               *
-*                                                       *
-* Execution : Do a XOR operation between two char array *
-*********************************************************/
-void XorOp(char * S1, const char * S2, const int _size){
-  for(int i = 0; i< _size; ++i){
-    S1[i] = S1[i] ^ S2[i];
-  }
-}
 
 void setup() {
   /*******************************
   * Init of Serial communication *
   ********************************/
-  Serial.begin(115200);
+  Serial.begin(19200);
   SerialLoRa.begin(19200);
   while (!Serial)
     delay(10);     // will pause Zero, Leonardo, etc until serial console opens
   while(!SerialLoRa)
     delay(10);
-
-  /*********************
-  * Decrypting the key *
-  **********************/
-  XorOp(key, Crypt, 16);
-  appKey = Binary_to_String(key, 16);
   
-  /********************************************
-  * Waiting for user to input Application key *
-  *********************************************/
-  Serial.println("Please input your AppKey :");
-  while(Serial.available() == 0);
-  String chaine = Serial.readString();
+  /********************************************************
+  * Looking in the nvm if the user already inserted a key *
+  *********************************************************/
+  String command = "AT$NVM 0\r\n";
+  SerialLoRa.print(command);
+  while(SerialLoRa.available() == 0);
+  String lorastr = SerialLoRa.readString();
+  
 
-  while(!chaine.equals(appKey + "\r\n")){
-    Serial.println("\nWrong App Key");
+  if(lorastr[4] != '1'){
+    /********************************************
+    * Waiting for user to input Application key *
+    *********************************************/
     Serial.println("Please input your AppKey :");
     while(Serial.available() == 0);
-    chaine = Serial.readString();
+    String chaine = Serial.readString();
+
+    /*******************************************************************
+    * Changing the flag so it won't ask another time to insert the key *
+    ********************************************************************/
+    command = "AT$NVM 0,1\r\n";
+    Serial.println(command);
+    SerialLoRa.print(command);
+    
+    /***********************************
+    * Writing the app key into the nvm *
+    ************************************/
+    for(int i=0; i<chaine.length() ; ++i){
+      command = "AT$NVM " + String(i+1) + "," + String((int)chaine[i]) + "\r\n";
+      Serial.println(command);
+      SerialLoRa.print(command);
+    }
+
+    /**************************************************
+    * Writing a char $ to mark the end of the app key *
+    ***************************************************/
+    command = "AT$NVM " + String(chaine.length()+1) + "," + String((int)'$') + "\r\n";
+    Serial.println(command);
+    SerialLoRa.print(command);
   }
 
+  
+  int ind = 0;
+  while(lorastr.substring(4,6).toInt() != (int)'$'){
+    /**************************************
+    * Retrieving the app key from the nvm *
+    ***************************************/
+    command = "AT$NVM " + String(ind) + "\r\n";
+    Serial.println(command);
+    SerialLoRa.print(command);
+    while(SerialLoRa.available() == 0);
+    lorastr = SerialLoRa.readString();
+
+    /**********************************************************
+    * Parsing only the important part of LoRa returned string *
+    ***********************************************************/
+    Serial.println(lorastr.substring(4,6));
+    Serial.println((char)lorastr.substring(4,6).toInt());
+    appKey += (char)lorastr.substring(4,6).toInt();
+    ++ind;
+  }
+  
+  /************************************************************************
+  * Removing the first and last character of nvm (flag and end of appkey) *
+  *************************************************************************/
+  Serial.println(appKey.substring(1,appKey.length()-1));
+  Serial.println("6EA8568CA821F024293C5C4B06679A4E");
+  
   /*******************************************
   * Starting modem with Europe configuration *
   ********************************************/
@@ -93,22 +107,25 @@ void setup() {
   Serial.print("Your device EUI is: ");
   Serial.println(modem.deviceEUI());
 
-  /*****************************************************
-  * Starting to write Application key inside the modem *
-  ******************************************************/
-  for(int i=0; i<appKey.length(); ++i){
-    SerialLoRa.write("AT$NVM " + i + key[i]);
-  }
+  /********************************************
+  * Blocking Application key inside the modem *
+  *********************************************/
+  command = "AT$APKACCESS\r\n";
+  SerialLoRa.print(command);
 
   /**********************************************************
   * Connecting the modem to a LoRaWAN network (like Helium) *
   ***********************************************************/
+  /*
   int connected = modem.joinOTAA(appEui, appKey);
   if (!connected) {
     Serial.println("Something went wrong; are you indoor? Move near a window and retry");
     while (1) {}
-  }
+  }*/
 
+  /***************************************************
+  * Setting modem rate to send data every 15 seconds *
+  ****************************************************/
   modem.dataRate(3);
 }
 
@@ -126,7 +143,7 @@ void loop() {
     char c = SerialLoRa.read();
     Serial.print(c);
   }
-
+  
   /*
   float t = sht31.readTemperature();
   float h = sht31.readHumidity();
@@ -142,9 +159,10 @@ void loop() {
   } else {
     Serial.println("Failed to read humidity");
   }
-
-  String msg = Serial.readStringUntil('\n');
-  //String msg = String(32);//String(t,2);
+  */
+  /*
+  //String msg = Serial.readStringUntil('\n');
+  String msg = String(32);//String(t,2);
 
   Serial.println();
   Serial.print("Sending: " + msg + " - ");
@@ -156,7 +174,7 @@ void loop() {
   Serial.println();
 
   int err = 0;
-  /*
+  
   modem.beginPacket();
   modem.print(msg);
   err = modem.endPacket(true);
